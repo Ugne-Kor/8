74d2437ecac01e503c9a463ea160ac1484168e14
Ugne-Kor <161746088+Ugne-Kor@users.noreply.github.com>
2024-02-29 18:43:24 +0200

    Add files via upload

---
+++ 8_uzd.R
@@ -0,0 +1,9 @@
+# Funkcijos surušiuoja failą pagal metus ir šalies pavadinimą, pašalina stebinius, kurių daugiau nei 50% reikšmės yra tuščios
+
+#Perskaito duomenų failą "datapol.cvs"
+data_pol <- read.csv("C:/Users/ugnek/OneDrive - Kaunas University of Technology/2 semestras/R/datapol_2.csv")
+#Išrikiuoja duomenų failą pagal metus ir šalies pavadinimą.
+data_pol <- arrange(data_pol, year, country, politics)
+#Pašalina stebinius, kurių daugiau nei 50% reikšmės yra tuščios.
+data_pol <- data_pol[, colSums(is.na(data_pol))<(nrow(data_pol)/2)]
+
